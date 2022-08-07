0d9b1a0dd7c5e7fe3b2ef3c38117e38443614b5e
Bolaji5821 <oyedelebolaji84@gmail.com>
2022-08-06 20:18:59 -0700

    hot fix

--- models/base_model.py
+++ models/base_model.py
@@ -1,56 +1,60 @@
 #!/usr/bin/env bash
 """
-A module that for the BaseModel class 
+A module that for the BaseModel class
 """
 
 
 from uuid import uuid4
 from datetime import datetime
+from models import storage
+
 
 class BaseModel:
+
     """
-    BaseModel: A class that defines all common attributes/methods for other classes
-    
+    BaseModel: A class that defines all common attributes/methods for
+    other classes
     """
+
     def __init__(self, *args, **kwargs):
+
         """
         Initializing the BaseModel class
-        
         """
+
         if not kwargs:
             self.id = str(uuid4())
             self.created_at = self.updated_at = datetime.now()
+            storage.new(self)
         else:
             for key, value in kwargs.items():
                 if key != '__class__':
-                    if key in('created_at', 'updated_at'):
+                    if key in ('created_at', 'updated_at'):
                         setattr(self, key, datetime.fromisoformat(value))
                     else:
-                        setattr(self,key, value)
+                        setattr(self, key, value)
+
     def __str__(self):
         """
         prints the string representation of BaseModel.
          [<class name>] (<self.id>) <self.__dict__>
+        """
+        return '[{}] ({}) {}'.format(type(self).__name__, self.id, self.__dict__)
 
-         """
-        return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
     def save(self):
-         """
-         updates the public instance attribute updated_at with the current date and time
-         
-         """
-         self.updated_at = datetime.now()
-
+        """
+        updates the public instance attribute updated_at with the
+        current date and time
+        """
+        self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """
         Returns a dictionary containing all keys/values of the instance
-        
         """
         dict_1 = self.__dict__.copy()
         dict_1["__class__"] = self.__class__.__name__
-        for k, v in self.__dict__.items():
-            if k in ("created_at", "updated_at"):
-                v = self.__dict__[k].isoformat()
-                dict_1[k] = v
-                return dict_1
+        dict_1["created_at"] = dict_1["created_at"].isoformat()
+        dict_1["updated_at"] = dict_1["updated_at"].isoformat()
+        return dict_1
---
+++ models/engine/file_storage.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+storage module to manage serialization and deserialization of JSON format.
+"""
+
+import json
+import os
+
+class FileStorage:
+    """A class  that serializes instances to a JSON file and deserializes JSON
+    file to instances
+    """
+
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """
+        Returns the dictionary __objects
+        """
+        return self.__objects
+
+    def new(self, obj):
+        """
+        sets in __objects the `obj` with key <obj class name>.id
+        """
+        self.__objects["{}.{}".format(obj.__class__.__name__, obj.id)] = obj
+
+    def save(self):
+        """
+        Serialize __objects to the JSON file
+        """
+        with open(self.__file_path, mode="w") as f:
+            dict_storage = {}
+            for k, v in self.__objects.items():
+                dict_storage[k] = v.to_dict()
+            json.dump(dict_storage, f)
+    def reload(self):
+        """Deserializes JSON file into __objects."""
+        from models.base_model import BaseModel
+        if not os.path.isfile(FileStorage.__file_path):
+            return
+        with open(FileStorage.__file_path, "r", encoding="utf-8") as f:
+            obj_dict = json.load(f)
+            obj_dict = {k: self.classes()[v["__class__"]](**v)
+                        for k, v in obj_dict.items()}
+            # TODO: should this overwrite or insert?
+            FileStorage.__objects = obj_dict
