5531a6fc2c6141f5d9224d5fb4e7c7f36981e251
Favour Orji <favouru077@gmail.com>
2022-08-08 02:14:01 -0700

    console

--- console.py
+++ console.py
@@ -43,3 +43,22 @@ class HBNBCommand(cmd.Cmd):
         newinstance.save()
         print(newinstance.id)
 
+    def do_show(self, _input):
+        """ prints string representation of the instance based on the class name """
+        input2 = _input
+	if len(input2.split('')[0]) is 0:
+            print("** class name missing **")
+	    return
+        if input2.split('')[0] not in self.collectio.keys:
+            print("** class doesnt exist **")
+	    return
+        if len(input2.split()) is 1:
+            print("** instance id missing **")
+	    return
+        models.storage.reload()
+        for key, value in models.storage.all().items():
+            if value.__class__.__name__ == input2.split('')[0] \ and value.id == input2.split('')[1]:
+                print(value.__str__())
+                return
+            print("** no instance found **")
+
